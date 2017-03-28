4eda8a49636a4fbcad3b1393947ea77a897ed603
Christopher Irvine <Christopher Irvine>
2017-03-28 19:59:08 +0000

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Prog2_CWK3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prog2_CWK3.dir/main.cpp.o"
+  "Prog2_CWK3.pdb"
+  "Prog2_CWK3.exe"
+  "libProg2_CWK3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prog2_CWK3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Prog2_CWK3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Prog2_CWK3.
+# This may be replaced when dependencies are built.
